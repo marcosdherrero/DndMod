c08a5155eca5970057e35d8d7a66daad60782f37
Marquitos <marcosdherrero@gmail.com>
2022-09-08 13:16:11 -0400

    9/8 updated villagers still does not add all the trades

--- src/main/java/net/marcos/dndmod/villager/ModVillagers.java
+++ src/main/java/net/marcos/dndmod/villager/ModVillagers.java
@@ -1,8 +1,11 @@
 package net.marcos.dndmod.villager;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.object.builder.v1.villager.VillagerProfessionBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.marcos.dndmod.DnDMod;
@@ -12,11 +15,13 @@ import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.InteractionObserver;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -25,9 +30,10 @@ import net.minecraft.world.World;
 import net.minecraft.world.poi.PointOfInterestType;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class ModVillagers extends VillagerEntity {
+public class ModVillagers extends VillagerEntity implements InteractionObserver, VillagerDataContainer {
 
     public static final PointOfInterestType CUSTOM_POI_ONE =                                                            //Adds Our Custom Point of Interest One
             registerPOI("custom_poi_one",                                                                        //registerPOI parameters (String name, Block block)
@@ -40,10 +46,38 @@ public class ModVillagers extends VillagerEntity {
                             Registry.POINT_OF_INTEREST_TYPE_KEY,
                             new Identifier(DnDMod.MOD_ID, "custom_poi_one")));
 
-    public ModVillagers(EntityType<? extends VillagerEntity> type, World world){                                        //Constructor
+
+    public static TradeOffers.Factory[] DICE_MAN_FACTORY = new TradeOffers.Factory[]{
+            new BuyItemForItemFactory(ModItems.D_ONE_HUNDRED, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_TWENTY, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_TWELVE, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_TEN, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_EIGHT, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_SIX, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_FOUR, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
+            new BuyItemForItemFactory(ModItems.D_TWO, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f)};
+
+    public static final Map DICE_MAN_MAPPED =
+            Util.make(Maps.newHashMap(), (map) -> {
+                map.put(CUSTOM_DICE_PROFESSION, copyToFastUtilMap(ImmutableMap.of(1, DICE_MAN_FACTORY)));
+            });
+
+    public static TradeOfferList DICE_OFFER_LIST;
+
+    public ModVillagers(EntityType<? extends VillagerEntity> entityType, World world){                                        //Constructor
         super(EntityType.VILLAGER, MinecraftClient.getInstance().world);
+        setDiceOfferList();
+        setOffers(DICE_OFFER_LIST);
+        fillRecipesFromPool(this.getOffers(), DICE_MAN_FACTORY, 8);
     }
-    
+
+    private void setDiceOfferList() {
+        for(int i = 0; i < DICE_MAN_FACTORY.length; i++){
+            DICE_OFFER_LIST.add(DICE_MAN_FACTORY[i].create(this,this.random));
+        }
+    }
+
+
     public static VillagerProfession registerProfession(String name, RegistryKey<PointOfInterestType> type) {           //Method to Register the Custom Villager Profession
         return Registry.register(                                                                                       //return Registry.register(registry, RegistryKey.of(registry.registryKey, id), entry);
                 Registry.VILLAGER_PROFESSION,                                                                           //registry of KEY
@@ -54,6 +88,7 @@ public class ModVillagers extends VillagerEntity {
                         .workSound(SoundEvents.ENTITY_VILLAGER_WORK_CLERIC)                                             //Set WorkSounds
                         .harvestableItems(ModItems.CUSTOM_SEEDS_ITEM, ModItems.CUSTOM_VEGETABLE_ITEM)                   //Set Harvestable Items
                         .build());
+
     }
 
     public static PointOfInterestType registerPOI(String name, Block block) {                                           //Method to Register a Block as a Point of Interest
@@ -64,24 +99,45 @@ public class ModVillagers extends VillagerEntity {
                 ImmutableSet.copyOf(                                                                                    //Create a Set from a copy of
                         block.getStateManager().getStates()));                                                          //All BlockState of these blocks
     }
-    public static void registerTrades(){
-        TradeOfferHelper.registerVillagerOffers(
-                CUSTOM_DICE_PROFESSION, 1, factories -> {
-                    factories.addAll(List.of(DICE_MAN_FACTORY));
-                } );
 
+    private static Int2ObjectMap<TradeOffers.Factory[]> copyToFastUtilMap(ImmutableMap<Integer, TradeOffers.Factory[]> map) {
+        return new Int2ObjectOpenHashMap(map);
     }
 
-    public static TradeOffers.Factory[] DICE_MAN_FACTORY = new TradeOffers.Factory[]{
-       new BuyItemForItemFactory(ModItems.D_ONE_HUNDRED, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_TWENTY, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_TWELVE, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_TEN, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_EIGHT, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_SIX, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_FOUR, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-       new BuyItemForItemFactory(ModItems.D_TWO, 1, ModItems.CUSTOM_ITEM, 16,1,30, 0.5f),
-    };
+    public static void registerTrades(){
+        TradeOffers.PROFESSION_TO_LEVELED_TRADE.putAll(DICE_MAN_MAPPED);
+    }
+
+    @Override
+    public void setOffers(TradeOfferList offers) {
+        this.offers = offers;
+    }
+
+    @Override
+    protected void fillRecipes() {
+        VillagerData villagerData = this.getVillagerData();
+        Int2ObjectMap<TradeOffers.Factory[]> int2ObjectMap = TradeOffers.PROFESSION_TO_LEVELED_TRADE.get(CUSTOM_DICE_PROFESSION);
+        if(int2ObjectMap != null && !int2ObjectMap.isEmpty()){
+            TradeOffers.Factory[] factory = int2ObjectMap.get(villagerData.getLevel());
+            if(factory != null){
+                TradeOfferList tradeOfferList = this.getOffers();
+                this.fillRecipesFromPool(tradeOfferList,factory,8);
+            }
+        }
+    }
+    @Nullable
+    @Override
+    public void fillRecipesFromPool(TradeOfferList recipeList, TradeOffers.Factory[] pool, int count) {
+        Set<Integer> set = Sets.newHashSet();
+        TradeOffers.Factory factory;
+        TradeOffer tradeOffer;
+        for(int i = 0; i< pool.length; i++){
+                set.add(i);
+                factory = pool[i];
+                tradeOffer = factory.create(this, this.random);
+                recipeList.add(tradeOffer);
+        }
+    }
 
     static class BuyItemForItemFactory implements TradeOffers.Factory{
         private final Item buy;
@@ -105,27 +161,9 @@ public class ModVillagers extends VillagerEntity {
         @Override
         public TradeOffer create(Entity entity, Random random) {
             ItemStack buyItem = new ItemStack(this.buy, this.buyAmount);
-            ItemStack sellItem = new ItemStack(this.buy, this.sellAmount);
-            return new TradeOffer(buyItem, sellItem, this.maxUses, this.experience, this.multiplier);
+            ItemStack sellItem = new ItemStack(this.sell, this.sellAmount);
+            return new TradeOffer(sellItem, buyItem, this.maxUses, this.experience, this.multiplier);
         }
-    }
-    @Override
-    protected void fillRecipes() {
-        VillagerData villagerData = this.getVillagerData();
-        Int2ObjectMap<TradeOffers.Factory[]> int2ObjectMap = TradeOffers.PROFESSION_TO_LEVELED_TRADE.get(villagerData.getProfession());
-        if (int2ObjectMap == null || int2ObjectMap.isEmpty()) {
-            return;
-        }
-        TradeOffers.Factory[] factories = int2ObjectMap.get(villagerData.getLevel());
-        if (factories == null) {
-            return;
-        }
-        TradeOfferList tradeOfferList = this.getOffers();
-        this.fillRecipesFromPool(tradeOfferList, factories, 8);
-    }
-    @Override
-    protected void fillRecipesFromPool(TradeOfferList recipeList, TradeOffers.Factory[] pool, int count) {
-        super.fillRecipesFromPool(recipeList, pool, 8);
     }
 
     public static void registerVillagers() {                                                                            //Connects the ModVillager Class to the Main Mod Class
